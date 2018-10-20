ddecf28fa1cbb38ec818d0f87f823df6ec5d2a0a
Mikhail Baranov <mikhail.baranov@gmail.com>
2018-10-20 15:27:13 +0300

    Test vehicle repository using mock

--- src/repositories/asyncpg/tests/test_vehicles.py
+++ src/repositories/asyncpg/tests/test_vehicles.py
@@ -1,45 +1,28 @@
-import pytest
-from asyncpgsa import pg
+from asyncpgsa.testing import MockPG
 
 from repositories.asyncpg.vehicles import VehiclePGRepository
 
-HOST = 'localhost'
-PORT = 5432
-DB_NAME = 'vehicle_builder'
-USER = 'postgres'
-PASS = 'password'
 
-
-async def init():
-    await pg.init(
-        host=HOST,
-        port=PORT,
-        database=DB_NAME,
-        user=USER,
-        password=PASS,
-        min_size=5,
-        max_size=10
-    )
-
-
-@pytest.mark.skip
 class TestVehiclesRepository:
     async def test_returns_vehicles_list(self):
-        await init()
+        pg = MockPG()
+        pg.set_database_results([{"id": 1, "title": "Car", "range": 100}])
         repository = VehiclePGRepository(pg)
         vehicles = await repository.get_all()
 
         assert len(vehicles) > 0
 
     async def test_returns_vehicle_by_id(self):
-        await init()
+        pg = MockPG()
+        pg.set_database_results([{"id": 1, "title": "Car", "range": 100}])
         repository = VehiclePGRepository(pg)
         vehicle = await repository.find_by_id(1)
 
         assert vehicle.id == 1
 
     async def test_returns_none_if_nothing_found(self):
-        await init()
+        pg = MockPG()
+        pg.set_database_results([])
         repository = VehiclePGRepository(pg)
         vehicle = await repository.find_by_id(100500)
 
