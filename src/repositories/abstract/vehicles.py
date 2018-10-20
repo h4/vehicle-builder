9d3c4f33072d85e0ac368e5594922229739a6c3e
Mikhail Baranov <mikhail.baranov@gmail.com>
2018-10-20 16:01:52 +0300

    Increase coverage little bit

--- src/repositories/abstract/vehicles.py
+++ src/repositories/abstract/vehicles.py
@@ -6,13 +6,10 @@ from entities.vehicle import Vehicle
 
 class VehicleRepositoryABC(ABC):
     @abstractmethod
-    def save(self, vehicle: Vehicle):
-        pass
+    def save(self, vehicle: Vehicle): pass
 
     @abstractmethod
-    def get_all(self) -> List[Vehicle]:
-        pass
+    def get_all(self) -> List[Vehicle]: pass
 
     @abstractmethod
-    def find_by_id(self, id: int) -> Vehicle:
-        pass
+    def find_by_id(self, pk: int) -> Vehicle: pass
